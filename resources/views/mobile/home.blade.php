ee024de454f58750e5bf04492f5ddf0154929dec
asrani <asrani.27@gmail.com>
2025-04-10 10:22:43 +0800

    f

--- resources/views/mobile/home.blade.php
+++ resources/views/mobile/home.blade.php
@@ -92,7 +92,7 @@
 
 <div class="p-2">
     <div class="d-flex justify-content-between align-items-center mb-2">
-        <span class="text-lg font-weight-bold"> DRAMA CHINA</span>
+        <span class="text-lg font-weight-bold"> DRAMA CHINA </span>
         <a href="" class="text-lime" style="white-space: nowrap;">Selengkapnya</a>
     </div>
     <div class="d-flex overflow-auto" style="gap: 0.5rem;">
