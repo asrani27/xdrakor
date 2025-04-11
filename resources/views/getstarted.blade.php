2565771b432da03216b6a471df90404852a5f513
asrani <asrani.27@gmail.com>
2025-04-11 23:02:33 +0800

    f

--- resources/views/getstarted.blade.php
+++ resources/views/getstarted.blade.php
@@ -50,6 +50,11 @@
             font-weight: bold;
         }
     </style>
+
+    <!-- PWA  -->
+    <meta name="theme-color" content="#6777ef" />
+    <link rel="apple-touch-icon" href="{{ asset('logo.png') }}">
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
 </head>
 
 <body>
@@ -60,6 +65,24 @@
 
     <script src="/muvnix/plugins/jquery/jquery.min.js"></script>
     <script src="/muvnix/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <script src="{{ asset('/sw.js') }}"></script>
+    <script>
+        if ("serviceWorker" in navigator) {
+      // Register a service worker hosted at the root of the
+      // site using the default scope.
+      navigator.serviceWorker.register("/sw.js").then(
+      (registration) => {
+         console.log("Service worker registration succeeded:", registration);
+      },
+      (error) => {
+         console.error(`Service worker registration failed: ${error}`);
+      },
+    );
+  } else {
+     console.error("Service workers are not supported.");
+  }
+    </script>
 </body>
 
 </html>
