e23f631ce4e4976d412a7277af7ee358ac231dd9
asrani <asrani.27@gmail.com>
2025-04-12 22:12:09 +0800

    f

--- resources/views/mobile/app.blade.php
+++ resources/views/mobile/app.blade.php
@@ -105,6 +105,14 @@
                                 </p>
                             </a>
                         </li>
+                        <li class="nav-item">
+                            <a href="/logout" class="nav-link">
+                                <i class="nav-icon fas fa-sign-out"></i>
+                                <p>
+                                    Logout
+                                </p>
+                            </a>
+                        </li>
                     </ul>
                 </nav>
                 <!-- /.sidebar-menu -->
