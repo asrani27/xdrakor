1ffcf1cea133fd0c1bcec147a6c83284afde0254
asrani <asrani.27@gmail.com>
2025-04-12 21:21:42 +0800

    f

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -6,6 +6,7 @@ use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Session;
+use Laravel\Socialite\Facades\Socialite;
 use DerekCodes\TurnstileLaravel\TurnstileLaravel;
 
 class LoginController extends Controller
